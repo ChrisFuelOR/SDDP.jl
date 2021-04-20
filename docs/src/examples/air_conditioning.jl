1049f26e6ee3b3194e59684591f43968df159c0a
ChrisFuelOR <72441442+ChrisFuelOR@users.noreply.github.com>
2021-04-20 14:55:47 +0200

    Small improvements

--- docs/src/examples/air_conditioning.jl
+++ docs/src/examples/air_conditioning.jl
@@ -28,7 +28,7 @@ function air_conditioning_model(integrality_handler, iteration_limit, solver)
         optimizer = GLPK.Optimizer,
         integrality_handler = integrality_handler,
     ) do sp, stage
-        @variable(sp, 0 <= stored_production <= 100, Int, SDDP.State, initial_value = 0, epislon = binaryPrecision)
+        @variable(sp, 0 <= stored_production <= 100, Int, SDDP.State, initial_value = 0)
         @variable(sp, 0 <= production <= 200, Int)
         @variable(sp, overtime >= 0, Int)
         @variable(sp, demand)
